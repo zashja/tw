2b22eac8d8d130699439afeccb156f8106dce2ce
lmatt <lmattiaccio@wbpro.com>
2024-07-26 14:58:22 -0300

    update

--- chat/script.js
+++ chat/script.js
@@ -1,29 +1,32 @@
-// Configuración del cliente de TMI.js
-const client = new tmi.Client({
-    connection: {
-        secure: true,
-        reconnect: true
-    },
-    channels: [ 'tu_canal_de_twitch' ]
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const client = new tmi.Client({
+        connection: {
+            secure: true,
+            reconnect: true
+        },
+        channels: ['zashja76'] // Reemplaza con el nombre correcto de tu canal
+    });
 
-// Conectarse al chat de Twitch
-client.connect();
+    client.connect();
 
-// Escuchar los mensajes
-client.on('message', (channel, tags, message, self) => {
-    if(self) return;
+    client.on('connected', (address, port) => {
+        console.log(`* Conectado a ${address}:${port}`);
+    });
 
-    const chatContainer = document.getElementById('chat-messages');
+    client.on('message', (channel, tags, message, self) => {
+        if (self) return;
 
-    // Crear un nuevo mensaje
-    const chatMessage = document.createElement('li');
-    chatMessage.classList.add('chat-message');
-    chatMessage.innerHTML = `<span class="username">${tags['display-name']}</span><span class="message">${message}</span>`;
+        const chatContainer = document.getElementById('chat-messages');
 
-    // Añadir el mensaje al contenedor
-    chatContainer.appendChild(chatMessage);
+        // Crear un nuevo mensaje
+        const chatMessage = document.createElement('li');
+        chatMessage.classList.add('chat-message');
+        chatMessage.innerHTML = `<span class="username">${tags['display-name']}</span><span class="message">${message}</span>`;
 
-    // Desplazar hacia abajo para ver el nuevo mensaje
-    chatContainer.scrollTop = chatContainer.scrollHeight;
+        // Añadir el mensaje al contenedor
+        chatContainer.appendChild(chatMessage);
+
+        // Desplazar hacia abajo para ver el nuevo mensaje
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+    });
 });
